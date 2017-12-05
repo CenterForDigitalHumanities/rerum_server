110b6b925cba449c69c22d7fa5697d550e9fc51f
Bryan Haberberger <bhaberbe@slu.edu>
2017-12-05 10:17:28 -0600

    Stop ignoring lib components, URLs set up for dev server instance.

--- src/java/edu/slu/action/AnnotationAction.java
+++ src/java/edu/slu/action/AnnotationAction.java
@@ -674,7 +674,9 @@ public class AnnotationAction extends ActionSupport implements ServletRequestAwa
                 String newObjectID = mongoDBService.save(Constant.COLLECTION_ANNOTATION, dbo);
                 //set @id from _id and update the annotation
                 BasicDBObject dboWithObjectID = new BasicDBObject((BasicDBObject)dbo);
-                String uid = "http://store.rerum.io/rerumserver/id/" + dboWithObjectID.getObjectId("_id").toString();
+
+                String uid = "http://devstore.io/rerumstore/id/" + dboWithObjectID.getObjectId("_id").toString();
+
                 dboWithObjectID.append("@id", uid);
                 mongoDBService.update(Constant.COLLECTION_ANNOTATION, dbo, dboWithObjectID);
                 JSONObject jo = new JSONObject();
