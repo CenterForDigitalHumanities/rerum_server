fe2fb7564c4aa473312b32118e0eca936bf6b246
Bryan Haberberger <bhaberbe@slu.edu>
2017-12-07 14:37:08 -0600

    Additional documentation.  Ready for discussion with @cubap.

--- src/java/edu/slu/action/AnnotationAction.java
+++ src/java/edu/slu/action/AnnotationAction.java
@@ -946,8 +946,11 @@ public class AnnotationAction extends ActionSupport implements ServletRequestAwa
      * The only way to hit the IIIF Validation API is to use the object's @id. The idea would be to save the objects to get an id, hit the 
      * IIIF API and if the object was intended to be IIIF, delete it from the store and return an error to the user.
      * 
-     * In the case the object was not intended to be IIIF, do not return an error.
+     * In the case the object was not intended to be IIIF, do not return an error. Since the methods calling this will handle what happens based of iiif_return.okay,
+     * just set okay to 1 and the methods calling this will treat it as if it isn't a problem. 
+     * 
      * @param objectToCheck A JSON object to parse through and validate.  This object has not yet been saved into mongo, so it does not have an @id yet
+     * @param intendedIIIF A flag letting me know whether or not this object is intended to be IIIF.  If it isn't, don't treat validation failure as an error.
      * @return iiif_return The return JSONObject from hitting the IIIF Validation API.
      */
     public JSONObject checkIIIFCompliance(JSONObject objectToCheck, boolean intendedIIIF) throws MalformedURLException, IOException{
@@ -957,7 +960,7 @@ public class AnnotationAction extends ActionSupport implements ServletRequestAwa
         iiif_return = checkIIIFCompliance(newObjectID, "2.1"); //If it is an object we are creating, this line means @context must point to Presentation API 2 or 2.1
         if(iiif_return.getInt("okay") == 0){
             if(intendedIIIF){
-                //If it was intended to be a IIIF object, then remove this object from the store because it was not valid and return an error to the user;
+                //If it was intended to be a IIIF object, then remove this object from the store because it was not valid and return an error to the user
                 BasicDBObject query = new BasicDBObject();
                 query.append("_id", newObjectID);
                 mongoDBService.delete(Constant.COLLECTION_ANNOTATION, query);
@@ -973,20 +976,20 @@ public class AnnotationAction extends ActionSupport implements ServletRequestAwa
      /**
      * A JSONObject that already has an @id can be validated against this IIIF validation URL.  It will return a JSONObject.
      * A save or update action could hit this to see if the resulting object is IIIF valid.  If not, a 'rollback' or 'delete' could be performed
-     * and the warnings and errors sent back to the user.
+     * and the warnings and errors sent back to the user.  If using this function, it is assumed objURL is intended to be a IIIF URL.
      * 
-     * @param objURL The @id or id of a IIIF JSON object.
-     * @param version The Intended Presentation API version to validate against.  (1, 2 or 2.1)
+     * @param objURL The @id or id URL of a IIIF JSON object represented as a String.
+     * @param version The Intended Presentation API version to validate against represented as a String.  (1, 2 or 2.1)
      * @return iiif_return The return JSONObject from hitting the IIIF Validation API.
      */
     public JSONObject checkIIIFCompliance(String objURL, String version) throws MalformedURLException, IOException{
         JSONObject iiif_return = new JSONObject();
         String iiif_validation_url = "http://iiif.io/api/presentation/validator/service/validate?format=json&version="+version+"&url="+objURL;
-        URL id = new URL(iiif_validation_url);
+        URL validator = new URL(iiif_validation_url);
         BufferedReader reader = null;
         StringBuilder stringBuilder;
-        HttpURLConnection connection = (HttpURLConnection) id.openConnection();
-        connection.setRequestMethod("GET");
+        HttpURLConnection connection = (HttpURLConnection) validator.openConnection();
+        connection.setRequestMethod("GET"); //hmm... I think this is right
         connection.setReadTimeout(15*1000);
         connection.connect();
         reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
