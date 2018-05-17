ecd4713bc3b28b812896ec9c255b1f1f289e408a
cubap <cubap@slu.edu>
2018-05-17 12:02:18 -0500

    already done, I bet.

--- src/java/edu/slu/action/ObjectAction.java
+++ src/java/edu/slu/action/ObjectAction.java
@@ -2260,7 +2260,7 @@ public class ObjectAction extends ActionSupport implements ServletRequestAware, 
             DecodedJWT d_jwt = verifier.verify(access_token);
             System.out.println("We were able to verify it. ");
             verified = true;
-            generatorID = recievedToken.getClaim("http://devstore.rerum.io/v1/agent").toString();
+            generatorID = recievedToken.getClaim("http://devstore.rerum.io/v1/agent").asString();
         } 
         catch (Exception exception){
             //Invalid signature/claims.  Try to authenticate the old way
