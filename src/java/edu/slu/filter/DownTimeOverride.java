a8130d8f0a192a2835a14682b2154ef67f750eb6
Bryan H <thehabes@github.io>
2019-07-31 11:01:56 -0500

    A global servlet interceptor to send a message during down time

--- src/java/edu/slu/filter/DownTimeOverride.java
+++ src/java/edu/slu/filter/DownTimeOverride.java
@@ -7,8 +7,6 @@ package edu.slu.filter;
 
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.MethodFilterInterceptor;
-import java.io.PrintWriter;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.struts2.ServletActionContext;
 
@@ -20,9 +18,7 @@ public class DownTimeOverride extends MethodFilterInterceptor {
 
     @Override
     protected String doIntercept(ActionInvocation ai) throws Exception {
-        HttpServletRequest request = ServletActionContext.getRequest();
         HttpServletResponse response = ServletActionContext.getResponse();
-        PrintWriter out = response.getWriter();
         String body = "We are performing maintainance right now.  Please try again later.  Sorry for the inconvenience.";
         response.setStatus(500);
         throw new Exception(body);
