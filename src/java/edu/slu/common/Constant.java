4877f530aa510ef5beeb010cc07a4265938f8571
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2020-08-25 14:22:54 -0500

    Test CI.

--- src/java/edu/slu/common/Constant.java
+++ src/java/edu/slu/common/Constant.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor. Test
  */
 package edu.slu.common;
 
