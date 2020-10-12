3e8355b5b4a2d0a446b8fc0d8049548b8343c72b
snsarma <shravya.sarma@gmail.com>
2020-10-12 16:05:18 -0400

    updateitem

--- src/edu/slu/action/ObjectAction.java
+++ src/edu/slu/action/ObjectAction.java
@@ -1885,7 +1885,7 @@ public class ObjectAction extends ActionSupport implements ServletRequestAware, 
             Table table = dynamoDB.getTable(tableName);
             
 UpdateItemSpec updateItemSpec = new UpdateItemSpec().withPrimaryKey("id", primarykey)
-                        .withValueMap(new ValueMap().withJSON("alpha", newjson.toString()));
+                        .withValueMap(new ValueMap().withJSON("alpha", newjson.toString())).withReturnValues(ReturnValue.ALL_NEW);;
 	            //.withJSON("alpha", newjson.toString());
 UpdateItemOutcome outcome = table.updateItem(updateItemSpec);
 
