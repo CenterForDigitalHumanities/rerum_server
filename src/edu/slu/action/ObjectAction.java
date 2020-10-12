ad7e1ec4058667b479570b5d62788f028a3f8fde
snsarma <shravya.sarma@gmail.com>
2020-10-12 18:56:43 -0400

    this is for configure rerum

--- src/edu/slu/action/ObjectAction.java
+++ src/edu/slu/action/ObjectAction.java
@@ -1881,7 +1881,7 @@ public class ObjectAction extends ActionSupport implements ServletRequestAware, 
                 }
                 //JSONObject newjson = new JSONObject();
                 newjson.putAll( data );
-                newjson = configureRerumOptions(newjson, true);
+                newjson = configureRerumOptions(received, true);
                 System.out.println("newjson in the putUpdate request:"+newjson);
                // System.out.println("id in the putUpdate request:"+json.get("id"));
             }
