dfe601b0b488e66773b78a25241eec7803fb506f
snsarma <shravya.sarma@gmail.com>
2020-10-17 19:49:48 -0400

    putUpdate

--- src/edu/slu/action/ObjectAction.java
+++ src/edu/slu/action/ObjectAction.java
@@ -1923,7 +1923,7 @@ public class ObjectAction extends ActionSupport implements ServletRequestAware, 
                 //JSONObject newjson = new JSONObject();
                 newjson.putAll( data );
                 //newjson = configureRerumOptions(received, true);
-               newjson = configureRerumOptions(newjson, true);
+               newjson = configureRerumOptions(newjson, false);
                 System.out.println("newjson in the putUpdate request:"+newjson);
                // System.out.println("id in the putUpdate request:"+json.get("id"));
             
