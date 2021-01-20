8d7c10b85dc128a37b217a44842b939fac168344
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2021-01-20 11:46:02 -0600

    Overwrite hotfix (#178)
    * This needs to be deployed on dev-01 and prd-02.
    
    * This needs to be deployed on dev-01 and prd-02.

--- src/java/edu/slu/action/ObjectAction.java
+++ src/java/edu/slu/action/ObjectAction.java
@@ -485,9 +485,7 @@ public class ObjectAction extends ActionSupport implements ServletRequestAware, 
             try{
                 annoToUpdate.getJSONObject("__rerum").getJSONObject("history").getJSONArray("next").add(newNextID); //write back to the anno from mongo
                 myAnnoWithHistoryUpdate = (DBObject)JSON.parse(annoToUpdate.toString()); //make the JSONObject a DB object
-                System.out.println("Update in Mongo");
                 mongoDBService.update(Constant.COLLECTION_ANNOTATION, myAnno, myAnnoWithHistoryUpdate); //update in mongo
-                System.out.println("Done");
                 altered = true;
             }
             catch(Exception e){ 
@@ -1871,11 +1869,10 @@ public class ObjectAction extends ActionSupport implements ServletRequestAware, 
                         JSONObject newObject = received;//The edited original object meant to be saved as a new object (versioning)
                         newObject.remove("_id");
                         JSONObject originalProperties = originalJSONObj.getJSONObject("__rerum");
-                        
                         LocalDateTime dt = LocalDateTime.now();
                         DateTimeFormatter dtFormat = DateTimeFormatter.ISO_DATE_TIME;
                         String formattedOverwrittenDateTime = dt.format(dtFormat);
-                        originalProperties.getJSONObject("__rerum").element("isOverwritten", formattedOverwrittenDateTime);
+                        originalProperties.element("isOverwritten", formattedOverwrittenDateTime);
                         newObject.element("__rerum", originalProperties);
                         DBObject udbo = (DBObject) JSON.parse(newObject.toString());
                         mongoDBService.update(Constant.COLLECTION_ANNOTATION, originalObject, udbo);
