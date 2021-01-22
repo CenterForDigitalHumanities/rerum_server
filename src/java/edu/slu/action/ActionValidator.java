0a557f96f9f6217f6cb17ffc2ec8b76c33b26d0f
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2021-01-22 15:10:25 -0600

    I have formed new opinions doing this.  However, this will work to validate things through RERUM without a connected API like the sandbox.  This means the sandbox should have a validator to for testing its endpoints.

--- src/java/edu/slu/action/ActionValidator.java
+++ src/java/edu/slu/action/ActionValidator.java
@@ -5,24 +5,26 @@
  */
 package edu.slu.action;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.util.JSON;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import edu.slu.common.Constant;
-import edu.slu.action.ObjectAction;
-import java.io.BufferedReader;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import edu.slu.common.Constant;
+import edu.slu.service.MongoDBService;
+import net.sf.json.JSONObject;
 
 /**
  *
  * @author bhaberbe
  */
 public class ActionValidator extends HttpServlet {
-
+    private MongoDBService mongoDBService;
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -35,15 +37,71 @@ public class ActionValidator extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
             request.setCharacterEncoding("UTF-8");
-            BufferedReader bodyReader = request.getReader();
-            StringBuilder bodyString = new StringBuilder();
-            String line;
-            String requestString;
-            StringBuilder sb = new StringBuilder();
             int codeOverwrite = 500;
             LocalDateTime dt = LocalDateTime.now();
             DateTimeFormatter dtFormat = DateTimeFormatter.ISO_DATE_TIME;
             String formattedOverwrittenDateTime = dt.format(dtFormat);
+            String createResult, putUpdateResult, patchUpdateResult, patchSetResult, patchUnsetResult, overwriteResult, deleteResult, queryResult;
+            createResult=putUpdateResult=patchUpdateResult=patchSetResult=patchUnsetResult=overwriteResult=deleteResult=queryResult="<font color='red'>ERROR</font>";
+            JSONObject jo = new JSONObject();
+            jo.element("validator", formattedOverwrittenDateTime);
+            DBObject test = (DBObject) JSON.parse(jo.toString());
+            
+            //For saving a new object, which is a save and update in mongo unit actions.  Both have to be working.
+            try{
+                String newObjectID = mongoDBService.save(Constant.COLLECTION_ANNOTATION, test);
+                //set @id from _id and update the annotation
+                BasicDBObject dboWithObjectID = new BasicDBObject((BasicDBObject)test);
+                String newid = Constant.RERUM_ID_PREFIX+newObjectID;
+                dboWithObjectID.put("@id", newid);
+                mongoDBService.update(Constant.COLLECTION_ANNOTATION, test, dboWithObjectID);
+                jo = JSONObject.fromObject(dboWithObjectID);
+                createResult = "<font color='green'>available</font>";
+            }
+            catch(Exception e){
+                createResult = "<font color='red'>unavailable</font>";
+            }
+            
+            //For PUT/PATCH/SET/UNSET/DELETE all use the same mongodbservice unit functionality.  findOneByExample - save - update.  OVERWRITE uses findOnByExample and update.
+            try{
+                String origID = Constant.RERUM_ID_PREFIX + "11111";
+                BasicDBObject query = new BasicDBObject();
+                query.append("@id", origID);
+                BasicDBObject originalObject = (BasicDBObject) mongoDBService.findOneByExample(Constant.COLLECTION_ANNOTATION, query); //The originalObject DB object
+                if(null != originalObject){
+                    jo.element("updateServices", "ok");
+                    test = (DBObject) JSON.parse(jo.toString());
+                    String newNextID = mongoDBService.save(Constant.COLLECTION_ANNOTATION, test);
+                    String newNextAtID = Constant.RERUM_ID_PREFIX+newNextID;
+                    BasicDBObject dboWithObjectID = new BasicDBObject((BasicDBObject)test);
+                    dboWithObjectID.append("@id", newNextAtID);
+                    jo.element("@id", newNextAtID);
+                    createResult=putUpdateResult=patchUpdateResult=patchSetResult=patchUnsetResult=deleteResult=overwriteResult="<font color='green'>available</font>";
+                }
+                else{
+                    putUpdateResult=patchUpdateResult=patchSetResult=patchUnsetResult=deleteResult=overwriteResult="<font color='red'>Error finding 11111</font>";
+                }
+            }
+            catch(Exception e){
+                putUpdateResult=patchUpdateResult=patchSetResult=patchUnsetResult=deleteResult=overwriteResult="<font color='red'>unavailable</font>";
+            }
+            
+            //For query, it uses findByExample
+            try{
+                String origID = Constant.RERUM_ID_PREFIX + "11111";
+                BasicDBObject query = new BasicDBObject();
+                query.append("@id", origID);
+                BasicDBObject originalObject = (BasicDBObject) mongoDBService.findByExample(Constant.COLLECTION_ANNOTATION, query);
+                if(null != originalObject){
+                    queryResult = "<font color='green'>available</font>";
+                }
+                else{
+                    queryResult = "<font color='red'>Error finding 11111</font>";
+                }
+            }
+            catch(Exception e){
+                queryResult = "<font color='red'>unavailable</font>";
+            }
             
             String generatedHTML = "<!DOCTYPE html>";
             generatedHTML += "<html>";
@@ -51,17 +109,25 @@ public class ActionValidator extends HttpServlet {
             generatedHTML += "<title> RERUM Endpoint Validator </title>";
             generatedHTML += "</head>";
             generatedHTML += "<body>";
-            generatedHTML += "<html>";
-            generatedHTML += "<html>";
-            generatedHTML += "<html>";
-            generatedHTML += "<html>";
+            generatedHTML += "<p>Below is a report on the avilability of RERUM endpoints as of "+formattedOverwrittenDateTime+"</p>";
+            generatedHTML += "<table><tr>";
+            generatedHTML += "<th>Action</th><th>Availability</th></tr>";
+            
+            generatedHTML += "<tr><td>Create</td><td>"+createResult+"</td></tr>"; //Create
+            generatedHTML += "<tr><td>Update (PUT)</td><td>"+putUpdateResult+"</td></tr>"; //Put Update
+            generatedHTML += "<tr><td>Update, Set, Unset (PATCH)</td><td>"+patchUpdateResult+"</td></tr>"; //Patch Update
+            generatedHTML += "<tr><td>Overwrite</td><td>"+overwriteResult+"</td></tr>"; //Overwrite
+            generatedHTML += "<tr><td>Delete</td><td>"+putUpdateResult+"</td></tr>"; //Delete
+            generatedHTML += "<tr><td>Query</td><td>"+queryResult+"</td></tr>"; //Query
+            generatedHTML += "</table>";
+             
             generatedHTML += "</body>";
             generatedHTML += "</html>";
             
           
             response.setHeader("Access-Control-Allow-Origin", "*");
             response.setStatus(codeOverwrite);
-            response.setHeader("Content-Type", "text/plain; charset=utf-8");
+            response.setHeader("Content-Type", "text/html; charset=utf-8");
             response.setCharacterEncoding("UTF-8");
             response.getWriter().print(generatedHTML);
     }
