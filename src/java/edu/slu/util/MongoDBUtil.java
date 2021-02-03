6d73b7cc329b5771b897b3fb91ad79cecb2eaa79
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2021-02-03 10:48:05 -0600

    Deploy error.  Need to fix ConnectionURI string to include trailing slash before options.

--- src/java/edu/slu/util/MongoDBUtil.java
+++ src/java/edu/slu/util/MongoDBUtil.java
@@ -35,14 +35,18 @@ public class MongoDBUtil {
 
     static {
         try {
+            System.out.println("Establishing Mongo Client to mongodb://f-vl-cdh-img-01:27017...");
             final MongoClientOptions options = MongoClientOptions.builder()
                     .connectionsPerHost(100)
                     .build();
-            MongoClientURI uri = new MongoClientURI(
-                    "mongodb://f-vl-cdh-img-01:27017?retryWrites=true&w=majority");
+            MongoClientURI uri = new MongoClientURI("mongodb://f-vl-cdh-img-01:27017/?retryWrites=true&w=majority");
             mg = new MongoClient(uri);
+            System.out.println("... Mongo Client Established");
+            System.out.println("Establishing annotationStore database connection...");
             db = mg.getDB("annotationStore");
+            System.out.println("Database is AnnotationStore");
         } catch (Exception e) {
+            System.out.println("!!! Mongo Client FAIL !!!");
             Logger.getLogger(MongoDBUtil.class.getName()).log(Level.SEVERE, null, e);
         }
     }
