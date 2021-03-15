a78b8b808e9cdce51f328732b75517e47536dc6c
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2021-03-15 14:57:03 -0500

    For #196 and #197

--- src/java/edu/slu/common/Constant.java
+++ src/java/edu/slu/common/Constant.java
@@ -13,10 +13,10 @@ public class Constant {
     public static final String RERUM_API_VERSION="1.0.0";
     
     //Mongo Connection String
-    public static final String DATABASE_CONNECTION = "mongodb://some-user:SomePassword@some-server:27017/DBNAME?w=majority&authMechanism=PICK-ONE";
+    public static final String DATABASE_CONNECTION = "mongodb://rerum-dev:69Trombones@f-vl-cdh-img-01:27017/annotationStoreDev?w=majority&authMechanism=SCRAM-SHA-256";
     
     //Mongo Database Name
-    public static final String DATABASE_NAME = "annotationStore"; // NOTE this changes between dev and prod
+    public static final String DATABASE_NAME = "annotationStoreDev"; // NOTE this changes between dev and prod.  Check the connection string.
     
     //Database Collection Name
     public static final String COLLECTION_ANNOTATION = "alpha"; //db.alpha.doStuff()
@@ -26,13 +26,13 @@ public class Constant {
     public static final String COLLECTION_V0 = "annotation"; // db.annotation.doStuff()
     
     //RERUM URL and endpoint paterns
-    public static final String RERUM_BASE="http://test-store.rerum.io";
-    public static final String RERUM_PREFIX="http://test-store.rerum.io/prd02-img01/";
-    public static final String RERUM_ID_PREFIX="http://test-store.rerum.io/prd02-img01/id/";
+    public static final String RERUM_BASE="http://devstore.rerum.io";
+    public static final String RERUM_PREFIX="http://devstore.rerum.io/v1/";
+    public static final String RERUM_ID_PREFIX="http://devstore.rerum.io/v1/id/";
     public static final String RERUM_AGENT_ClAIM="http://devstore.rerum.io/v1/agent";
     
     //RERUM API Linked Data context
-    public static final String RERUM_CONTEXT="http://test-store.rerum.io/prd02-img01/context.json";
+    public static final String RERUM_CONTEXT="http://devstore.rerum.io/v1/context.json";
     
     //The location of the public API documents.  This is necessary for JSON-LD context purposes.
     public static final String RERUM_API_DOC="https://github.com/CenterForDigitalHumanities/rerum_server/blob/master/API.md#__rerum";
