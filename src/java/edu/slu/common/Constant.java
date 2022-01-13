4dddf8497d30dd6558025961467399cc3f827a9a
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2022-01-13 14:54:53 -0600

    Stage for prod

--- src/java/edu/slu/common/Constant.java
+++ src/java/edu/slu/common/Constant.java
@@ -13,7 +13,7 @@ public class Constant {
     public static final String RERUM_API_VERSION="1.0.0";
     
     //Mongo Database Name
-    public static final String DATABASE_NAME = "annotationStoreDev"; // NOTE this changes between dev and prod.  Check the connection string.
+    public static final String DATABASE_NAME = "annotationStore"; // NOTE this changes between dev and prod.  Check the connection string.
 
     //Database Collection Name
     public static final String COLLECTION_ANNOTATION = "alpha"; //db.alpha.doStuff()
@@ -23,13 +23,13 @@ public class Constant {
     public static final String COLLECTION_V0 = "annotation"; // db.annotation.doStuff()
 
     //RERUM URL and endpoint paterns
-    public static final String RERUM_BASE="http://devstore.rerum.io";
-    public static final String RERUM_PREFIX="http://devstore.rerum.io/v1/";
-    public static final String RERUM_ID_PREFIX="http://devstore.rerum.io/v1/id/";
-    public static final String RERUM_AGENT_ClAIM="http://devstore.rerum.io/v1/agent";
+    public static final String RERUM_BASE="http://store.rerum.io";
+    public static final String RERUM_PREFIX="http://store.rerum.io/v1/";
+    public static final String RERUM_ID_PREFIX="http://store.rerum.io/v1/id/";
+    public static final String RERUM_AGENT_ClAIM="http://store.rerum.io/v1/agent";
 
     //RERUM API Linked Data context
-    public static final String RERUM_CONTEXT="http://devstore.rerum.io/v1/context.json";
+    public static final String RERUM_CONTEXT="http://store.rerum.io/v1/context.json";
 
     //The location of the public API documents.  This is necessary for JSON-LD context purposes.
     public static final String RERUM_API_DOC="https://github.com/CenterForDigitalHumanities/rerum_server/blob/master/API.md#__rerum";
