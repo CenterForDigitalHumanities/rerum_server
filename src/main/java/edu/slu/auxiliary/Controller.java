2ed4bfc9a3af7d0bc2ebd44ac4af5f7a112ed38d
cubap <cubap@slu.edu>
2022-01-31 16:35:51 -0600

    working on it

--- src/main/java/edu/slu/auxiliary/Controller.java
+++ src/main/java/edu/slu/auxiliary/Controller.java
@@ -8,13 +8,13 @@ package edu.slu.auxiliary;
 import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.logging.Logger;
+import edu.slu.auxiliary.CompactService;
 
 import net.sf.json.JSONObject;
 
 public class Controller {
     private static final Method expand = null;
     private static final Method compress = null;
-    private static final Method compact = null;
     private static final Method recurse = null;
     private static final Method batch = null;
     private static final Method decscribe = null;
@@ -22,9 +22,9 @@ public class Controller {
      * Map of API flags from the query string and the resulting method call.
      * Organized in order of operations?
      */
-    private final Map<String, Method> flagMap = Map.of(
+    private final Map<String, Class> flagMap = Map.of(
             "expand", expand, // awaiting methods
-            "compact", compact,
+            "compact", CompactService.Compactor(null),
             "recurse", recurse,
             "batch", batch,
             "decscribe", decscribe,
