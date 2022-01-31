877367c54dfddbdfdecc44f6af64d561d390a42b
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2022-01-31 15:38:52 -0600

    Add a polisher in case we decide to employ an array of keys to always remove no matter what.

--- src/main/java/edu/slu/auxiliary/CompressorService.java
+++ src/main/java/edu/slu/auxiliary/CompressorService.java
@@ -1,6 +1,14 @@
 /*
  * Gimme some JSON.  I'll give you back less JSON.
  * @author bhaberbe
+
+ * @notes
+ * In my mind, someone is calling this hoping RERUM knows how to compress it.
+ * If they were to go as far as providing the keys to be removed, they would just do it themselves.
+ * Instead, you provide the Compressor keys to keep, and let it go to work on your JSON objects.  
+ * We can have default keys we know we must keep.
+ * We can have default keys we know must bee removed.
+ * Anything else has to be provided.  Keys to keep can be provided.  
  */
 package edu.slu.auxiliary;
 
@@ -9,14 +17,8 @@ import java.util.Iterator;
 import net.sf.json.JSONObject;
 
 public class CompressorService {
+    //Bring in Global or Utility functionality from the auxiliary package.
     private Controller aux_controller = new Controller();
-    
-    //In my mind, someone is calling this hoping RERUM knows how to compress it.
-    //If they were to go as far as providing the keys to be removed, they would just do it themselves.
-    //Instead, you provide the Compressor keys to keep, and let it go to work on your JSON objects.  
-    //We can have default keys we know we must keep.
-    //We can have default keys we know must bee removed.
-    //Anything else has to be provided.  Keys to keep can be provided.  
     
     //These are keys that we want to make sure stay with the JSONObject.
     private final String[] primitiveKeys = 
@@ -34,7 +36,7 @@ public class CompressorService {
         };
     
     //These are keys that will be removed 100% of the time.  Maybe just __rerum.
-    private final String[] illegalKeys = {
+    private final String[] alwaysRemove = {
         "__rerum"
     };
     
@@ -46,7 +48,7 @@ public class CompressorService {
     }
     
     /**
-     * Initialize default compressing, where to object to compress is provided.
+     * Initialize default compressing, where the JSONObject to compress is provided.
      * @param toCompress A JSONObject to compress and return.
      * @return The Compressed JSONObject
      */
@@ -55,7 +57,7 @@ public class CompressorService {
     }
     
     /**
-     * Initialize the compressor, where the keys to keep are provided as an array
+     * Initialize the compressor, where the keys to keep are provided as a String[] array
      * @param toCompress A JSONObject to compress and return.
      * @param keysToKeep The keys of the JSONObject that should remain after compression.
      * @return The Compressed JSONObject
@@ -73,7 +75,7 @@ public class CompressorService {
      * Remove keys from a JSONObject that should not be kept because of compression.
      * This will remove every key from the provided JSONObject that is not in the provided array of keys to keep.
      * @param toCompress A JSONObject to compress (by removing unwanted keys)
-     * @param keysToKeep A String Array of keys that should not be removed.
+     * @param keysToKeep A String[] Array of keys that should not be removed.
      * @return toCompress once the keys are removed, or the original if there was an error.
      */
     private JSONObject compress(JSONObject toCompress, String[] keysToKeep){
@@ -91,6 +93,25 @@ public class CompressorService {
         }
         catch(Exception e){
             System.out.println("Could not compress object.  See error below.");
+            System.out.println(e);
+            return orig;
+        }
+    }
+    
+    private JSONObject polish(JSONObject o){
+        Iterator keys = o.keys();
+        JSONObject orig = JSONObject.fromObject(o);
+        try{
+            while(keys.hasNext()){
+                String key = (String)keys.next();
+                if(Arrays.asList(alwaysRemove).contains(key)){
+                    o.remove(key);
+                }
+            }
+            return o;
+        }
+        catch(Exception e){
+            System.out.println("Could not polish object.  See error below.");
             System.out.println(e);
             return orig;
         }
