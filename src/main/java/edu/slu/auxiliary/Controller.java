af5f0afbf5d0acdb93434dfe3996c9dc570833e8
cubap <cubap@slu.edu>
2022-01-31 14:49:51 -0600

    Update Controller.java

--- src/main/java/edu/slu/auxiliary/Controller.java
+++ src/main/java/edu/slu/auxiliary/Controller.java
@@ -14,26 +14,32 @@ import net.sf.json.JSONObject;
 public class Controller {
     private static final Method expand = null;
     private static final Method compress = null;
+    private static final Method compact = null;
+    private static final Method recurse = null;
+    private static final Method batch = null;
+    private static final Method decscribe = null;
     /**
      * Map of API flags from the query string and the resulting method call.
      * Organized in order of operations?
      */
     private final Map<String, Method> flagMap = Map.of(
-        "expand", expand, // awaiting methods
-        "compress", compress        
-    ); 
+            "expand", expand, // awaiting methods
+            "compact", compact,
+            "recurse", recurse,
+            "batch", batch,
+            "decscribe", decscribe,
+            "compress", compress);
 
-public JSONObject processFlags(JSONObject documObject, String[] params) {
-    try {
-        for( String param: params) {
-            if(flagMap.containsKey(param)) {
-                documObject = (JSONObject) flagMap.get(param).invoke(documObject);
+    public JSONObject processFlags(JSONObject documObject, String[] params) {
+        try {
+            for (String param : params) {
+                if (flagMap.containsKey(param)) {
+                    documObject = (JSONObject) flagMap.get(param).invoke(documObject);
+                }
             }
+        } catch (Exception e) {
+            Logger.getLogger(Controller.class.getName()).info("Could not process flag.\n" + e.getMessage());
         }
-    } catch (Exception e) {
-        Logger.getLogger(Controller.class.getName()).info("Could not process flag.\n"+e.getMessage());
+        return documObject;
     }
-    return documObject;
-}
-
 }
