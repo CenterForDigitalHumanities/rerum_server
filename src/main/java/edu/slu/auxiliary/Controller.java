53d013a7d5dcc7a28a6bfc6919f87dba9f5af71b
Patrick Cuba <cubap@slu.edu>
2022-02-01 16:50:47 -0600

    stubby
    This is broken until all the parts are in place.

--- src/main/java/edu/slu/auxiliary/Controller.java
+++ src/main/java/edu/slu/auxiliary/Controller.java
@@ -12,16 +12,12 @@ import java.util.logging.Logger;
 import net.sf.json.JSONObject;
 
 public class Controller {
-    private static final Method expand = null;
-    private static final Method compress = null;
-    private static final Method compact = null;
-    private static final Method recurse = null;
-    private static final Method batch = null;
-    private static final Method decscribe = null;
     private final JSONObject document;
+    private final Map<String,String[]> params;
     
-    public Controller(JSONObject json){
+    public Controller(JSONObject json, Map<String,String[]> queryParameters){
         document = json;
+        params = queryParameters;
     }
     /**
      * Map of API flags from the query string and the resulting method call.
@@ -35,33 +31,60 @@ public class Controller {
             "decscribe",
             "compress");
 
-    public JSONObject processFlags(JSONObject documObject, String[] params) {
-        Set<String> validFlags = params.stream()
+    public JSONObject processFlags() {
+        Set<String> validFlags = params.keyset()
+        .stream()
         .distinct()
         .filter(flagsList::contains)
         .collect(Collectors.toSet());
         if (validFlags.size()>0)
         {
-            if (validFlags.contains("expand"))
-            try {
-                // JSONObject serviced = new ServiceController().compress(jo);
-                // getting ready to if all these for Bryan
+            JSONObject servicedDocument = new JSONObject(document.toString());
+
+            try { // maybe everyone gets a try later
+                if (validFlags.contains("compress")) {
+                    servicedDocument = this.compress();
+                }
+                else if (validFlags.contains("compact")) {
+                    servicedDocument = this.compact();
+                }
+                else if (validFlags.contains("expand")){
+                    servicedDocument = this.expand();
+                }
             } catch (Exception e) {
                 Logger.getLogger(Controller.class.getName()).info("Could not process flag.\n" + e.getMessage());
             }
+            return servicedDocument;
         }
-        return documObject;
+        return document;
     }
     
     /**
      * Remove keys from a JSONObject that should not be kept because of compression.
      * This will remove every key from the provided JSONObject that is not in the array of default primitive keys.
-     * @param toCompress A JSONObject to compress (by removing unwanted keys)
-     * @param keysToKeep A String[] Array of keys that should not be removed.
-     * @return toCompress once the keys are removed, or the original if there was an error.
+     * @return compressed JSON or the original if there was an error.
      */
-    public JSONObject compress(String[] keysToKeep){
-        //Either you get keys to keep, or you pass in a null/empty which will just use the default
+    public JSONObject compress(){
+        Set<String> keysToKeep = params.get("key[]")
+        if(keysToKeep == null) {
+            return new CompressorService().compress(document);
+        }
         return new CompressorService().compress(document, keysToKeep);
+    }
+    
+    /**
+     * Reduce key values within a JSONObject.
+     * @return compacted JSON or the original if there was an error.
+     */
+    public JSONObject compact(){
+        return new CompactorService().compact(document);
+    }
+    
+    /**
+     * Expand internal references in JSONObject.
+     * @return expanded JSON or the original if there was an error.
+     */
+    public JSONObject expand(){
+        return new ExpansionService().expand(document);
     }
 }
