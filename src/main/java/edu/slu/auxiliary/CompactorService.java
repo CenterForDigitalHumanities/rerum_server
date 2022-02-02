fd2f237b32fd14694f85c05a7e8b68cdb35e86bb
Patrick Cuba <cubap@slu.edu>
2022-02-02 12:16:15 -0600

    minimizeReferences
    Not linted or check in JDK at all yet

--- src/main/java/edu/slu/auxiliary/CompactorService.java
+++ src/main/java/edu/slu/auxiliary/CompactorService.java
@@ -1,15 +1,85 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package edu.slu.auxiliary;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 import net.sf.json.JSONObject;
 
 /**
- *
- * @author bhaberbe
+ * Transform an object from the database into a version of the object without 
+ * metadata keys, with values reduced as much as possible, and with all full 
+ * or partially dereferenced reduced to their URI.
  */
-public interface CompactorService {
-    public JSONObject compact();
+public class CompactorService {
+    
+    /**
+     * Default, where the object to compact is provided.
+     * @param objectToCompact A JSONObject to compact and return.
+     * @return compacted JSONObject
+     */
+    public JSONObject compact(JSONObject objectToCompact){
+        JSONObject toReturn = stripProperties(objectToCompact);
+        toReturn = minimizeReferences(toReturn);
+        toReturn = simplifyValues(toReturn);
+        return toReturn;
+    }
+    
+    /**
+     * Simplify values to literal, if possible.
+     * @param objectToCompact A JSONObject to compress (by removing unwanted keys)
+     * @return toCompress once the keys are removed, or the original if there was an error.
+     */
+    private JSONObject simplifyValues(JSONObject objectToCompact) {
+        JSONObject orig = JSONObject.fromObject(objectToCompact);
+        return orig;
+    }
+
+    /**
+     * Simplify embedded objects to URI, if possible.
+     * @param objectToCompact A JSONObject to compress (by removing unwanted keys)
+     * @return toCompress once the keys are removed, or the original if there was an error.
+     */
+        private JSONObject minimizeReferences(JSONObject objectToCompact) {
+            Iterator values = objectToCompact.values();
+                while(values.hasNext()){
+                    <T> val = values.next();
+                    if(val instanceof Object){
+                        if val.keySet().contains("@id") {
+                            val = val.get("@id");
+                            continue;
+                        }
+                        
+                        if val.keySet().contains("id") {
+                            val = val.get("id");
+                        }   
+                    }
+                }
+                return objectToCompact;
+    }
+
+    /**
+     * Strip JSONObject properties that should not be returned.
+     * @param objectToCompact A JSONObject to compress (by removing unwanted keys)
+     * @return toCompress once the keys are removed, or the original if there was an error.
+     */
+    private JSONObject stripProperties(JSONObject objectToCompact){
+        JSONObject orig = JSONObject.fromObject(objectToCompact);
+        List<String> removeableProperties = Arrays.asList("__rerum","_id");
+        Iterator keys = objectToCompact.keys();
+        try{
+            while(keys.hasNext()){
+                String key = (String)keys.next();
+                if(removeableProperties.contains(key)){
+                    objectToCompact.remove(key);
+                }
+            }
+            return objectToCompact;
+        }
+        catch(Exception e){
+            System.out.println("Could not compact object.  See error below.");
+            System.out.println(e);
+            return orig;
+        }
+    }
 }
