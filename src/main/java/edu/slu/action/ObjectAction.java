1d03705c56dba28306dfe42e6adff9bf2dd342b1
Patrick Cuba <cubap@slu.edu>
2024-10-18 00:46:08 -0500

    Repair release (#240)
    * Update Controller.java
    
    * Update Controller.java
    
    * A starter to this whole compressor thing.  Pretty simple JSON key destruction, it's either naive or elegant.
    
    * Yeah force our primitive set into the supplied set.  We will make a hard choice for these keys and perhaps ENUM them.
    
    * Add a polisher in case we decide to employ an array of keys to always remove no matter what.
    
    * Add a polisher in case we decide to employ an array of keys to always remove no matter what.
    
    * Add a polisher in case we decide to employ an array of keys to always remove no matter what.
    
    * working on it
    
    * Preparing to test...gunna do some Class magic first.
    
    * Update Controller.java
    
    * Notes
    
    * Update Controller.java
    
    * stubby
    
    This is broken until all the parts are in place.
    
    * minimizeReferences
    
    Not linted or check in JDK at all yet
    
    * simplifyValues()
    
    disclaimed. Also, this is only looking at some values for now and only one level deep.
    
    * Update CompactorService.java
    
    mangled by an online validator, so it should be closer to working now.
    
    * enable update on release
    
    ---------
    
    Co-authored-by: Bryan Haberberger <bryan.j.haberberger@slu.edu>

--- src/main/java/edu/slu/action/ObjectAction.java
+++ src/main/java/edu/slu/action/ObjectAction.java
@@ -47,6 +47,7 @@
 
 package edu.slu.action;
 
+import edu.slu.auxiliary.*;
 import com.auth0.jwk.Jwk;
 import com.auth0.jwk.JwkException;
 import com.auth0.jwk.JwkProvider;
@@ -1505,6 +1506,11 @@ public class ObjectAction extends ActionSupport implements ServletRequestAware, 
                         }
                         response.setStatus(HttpServletResponse.SC_OK);
                         out = response.getWriter();
+                        //AUX services happen before the return.
+                        //By now, we know what parameters were passed in.  That may be a Set, List, or Map, may or may not be boolean.
+                        //Controller c = new Controller();
+                        
+                        jo = new CompressorService().compress(jo);
                         out.write(mapper.writer().withDefaultPrettyPrinter().writeValueAsString(jo));
                     } catch (IOException ex) {
                         Logger.getLogger(ObjectAction.class.getName()).log(Level.SEVERE, null, ex);
--- src/main/java/edu/slu/auxiliary/Controller.java
+++ src/main/java/edu/slu/auxiliary/Controller.java
@@ -1,13 +1,91 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package edu.slu.auxiliary;
-
 /**
  *
  * @author bhaberbe
+ * @author cubap
  */
+package edu.slu.auxiliary;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.logging.Logger;
+import edu.slu.auxiliary.CompactService;
+
+import net.sf.json.JSONObject;
+
 public class Controller {
+    private final JSONObject document;
+    private final Map<String,String[]> params;
     
+    public Controller(JSONObject json, Map<String,String[]> queryParameters){
+        document = json;
+        params = queryParameters;
+    }
+    /**
+     * Map of API flags from the query string and the resulting method call.
+     * Organized in order of operations?
+     */
+    private final List<String> flagsList = Array.asList(
+            "expand",
+            "compact",
+            "recurse",
+            "batch",
+            "decscribe",
+            "compress");
+
+    public JSONObject processFlags() {
+        Set<String> validFlags = params.keyset()
+        .stream()
+        .distinct()
+        .filter(flagsList::contains)
+        .collect(Collectors.toSet());
+        if (validFlags.size()>0)
+        {
+            JSONObject servicedDocument = new JSONObject(document.toString());
+
+            try { // maybe everyone gets a try later
+                if (validFlags.contains("compress")) {
+                    servicedDocument = this.compress();
+                }
+                else if (validFlags.contains("compact")) {
+                    servicedDocument = this.compact();
+                }
+                else if (validFlags.contains("expand")){
+                    servicedDocument = this.expand();
+                }
+            } catch (Exception e) {
+                Logger.getLogger(Controller.class.getName()).info("Could not process flag.\n" + e.getMessage());
+            }
+            return servicedDocument;
+        }
+        return document;
+    }
+    
+    /**
+     * Remove keys from a JSONObject that should not be kept because of compression.
+     * This will remove every key from the provided JSONObject that is not in the array of default primitive keys.
+     * @return compressed JSON or the original if there was an error.
+     */
+    public JSONObject compress(){
+        Set<String> keysToKeep = params.get("key[]")
+        if(keysToKeep == null) {
+            return new CompressorService().compress(document);
+        }
+        return new CompressorService().compress(document, keysToKeep);
+    }
+    
+    /**
+     * Reduce key values within a JSONObject.
+     * @return compacted JSON or the original if there was an error.
+     */
+    public JSONObject compact(){
+        return new CompactorService().compact(document);
+    }
+    
+    /**
+     * Expand internal references in JSONObject.
+     * @return expanded JSON or the original if there was an error.
+     */
+    public JSONObject expand(){
+        return new ExpansionService().expand(document);
+    }
 }
